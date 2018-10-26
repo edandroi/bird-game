c884b3d2e4c57f26bb0b975fe21057a8d9cb7350
Unknown <edanuramail@gmail.com>
2018-10-26 15:36:13 -0400

    PreGabe

---
+++ Assets/Scripts/LerpCenter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpCenter : MonoBehaviour
+{
+
+	public GameObject lerpObj;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, lerpObj.transform.position, Time.deltaTime*15);
+	}
+}
